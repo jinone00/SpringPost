3d989a5d4b15ce88cd5b1fe5ea2a2a347371faf2
Jinone <103496651+jinone00@users.noreply.github.com>
2023-07-19 20:14:15 +0900

    1

---
+++ src/main/java/com/example/post/controller/PostApiController.java
@@ -0,0 +1,28 @@
+package com.example.post.controller;
+
+import com.example.post.dto.PostRequestDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class PostApiController {
+
+    @PostMapping("/post")
+    public void post(@RequestBody Map<String, Object> requestData){
+        requestData.forEach((key, value) -> {
+            System.out.println("key : " + key);
+            System.out.println("value : " + value);
+        });
+    }
+
+    @PostMapping("/post2")
+    public void post2(@RequestBody PostRequestDto requestData2){
+        System.out.println(requestData2);
+    }
+
+}
